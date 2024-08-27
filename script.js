7bc4a6e4903dbb530b00be426ab8caf1d36845d3
Narges Hosseini <nargeshosseini199564@gmail.com>
2024-08-26 17:13:11 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function loadPage(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error loading page:', error);
+        });
+}
